66ff5638e53d2233ffe8dc9e93d8ffdc97ae75ee
whitecloak <whitecloak@ip-172-31-20-14.ap-southeast-1.compute.internal>
2019-03-26 08:53:02 +0000

    Fix Config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=(sticky-test.whitecloak.io www.sticky-test.whitecloak.io)
+domains=(sticky-dev.whitecloak.io )
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="jeanandrew.fuentes@whitecloak.io" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
